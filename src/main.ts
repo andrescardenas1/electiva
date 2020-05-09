adea422b48e363454c7147a5f72eb065f29cd78a
AndresCardenas <cardenas881@hotmail.com>
2020-05-09 18:49:37 -0500

    electiva

--- src/main.ts
+++ src/main.ts
@@ -10,3 +10,31 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+  let ubicacionPrincipal = window.pageYOffset; //0
+
+  window.addEventListener("scroll", function(){
+    let desplazamientoActual = window.pageYOffset; //180
+    if(ubicacionPrincipal >= desplazamientoActual){ // 200 > 180
+        document.getElementsByTagName("nav")[0].style.top = "0px"
+    }else{
+        document.getElementsByTagName("nav")[0].style.top = "-100px"
+    }
+    ubicacionPrincipal= desplazamientoActual; //200
+
+})
+
+let enlacesHeader = document.querySelectorAll(".enlaces-header")[0];
+let semaforo = true;
+
+document.querySelectorAll(".hamburguer")[0].addEventListener("click", function(){
+  if(semaforo){
+      document.querySelectorAll(".hamburguer")[0].animate.prototype ="#fff";
+      semaforo= false;
+  }else{
+      document.querySelectorAll(".hamburguer")[0].animate.prototype ="#000";
+      semaforo= true;
+  }
+
+  enlacesHeader.classList.toggle("menudos")
+})
